a76dc605640be6bc37c33ebfdd693982ff467c19
HEEPOKE <Damon1fx@gmail.com>
2023-05-02 11:11:39 +0700

    :money_with_wings: setup config

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,10 +2,11 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const { DATABASE_URL } = process.env;
+const { DATABASE_URL, PORT } = process.env;
 
 const config = {
     DATABASE_URL,
+    PORT,
 }
 
 export default config;
