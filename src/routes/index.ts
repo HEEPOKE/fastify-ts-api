76425ddaf942b455c95bd2e7089d81c003a97679
HEEPOKE <Damon1fx@gmail.com>
2023-05-02 16:33:22 +0700

    :money_with_wings: update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,6 +8,11 @@ async function routes(fastify: FastifyInstance, prefix: string) {
     });
 
     fastify.get(`${prefix}/account/list`, accountController.listAccount);
+    fastify.get(`${prefix}/account/get/:id`, accountController.getAccountById);
+    fastify.get(`${prefix}/account/find/:id`, accountController.findAccountByEmail);
+    fastify.post(`${prefix}/account/add`, accountController.createAccount);
+    fastify.put(`${prefix}/account/update/:id`, accountController.updateAccount);
+    fastify.delete(`${prefix}/account/delete/:id`, accountController.deleteAccount);
 }
 
 export default routes;
